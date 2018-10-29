3d1388193d37ae9047e29777fc2af6b7f3ecd672
Amadosi Odaibo <odaiboamadosi@gmail.com>
2018-10-29 19:40:34 +0100

    setting db values in right config file

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
     // setup CORS
     app.use((req, res, next) => {
         // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin','https://www.nauticplatform.com');
+        res.setHeader('Access-Control-Allow-Origin',['http://localhost:3000/','https://www.nauticplatform.com']);
         // Request methods you wish to allow
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         // Request headers you wish to allow
