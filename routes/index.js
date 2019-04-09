a3d6ec9768b3ecaa2b599786442fcc3b29345f00
Amadosi Odaibo <odaiboamadosi@gmail.com>
2019-04-09 18:59:23 +0100

    making the cors work

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const auth = require("./authentication");
 const v1 = require("./v1/index");
+const url = require('url');
 
 module.exports = (app) => {
     // setup CORS
@@ -7,12 +8,15 @@ module.exports = (app) => {
 
         const allowedOrigins = ['http://localhost:3000','https://www.nauticplatform.com'];
 
-        const origin = req.headers.referer || req.headers.origin;
+        const origin = req.headers.origin || req.headers.referer;
 
-        if(allowedOrigins.indexOf(origin) > -1){
-            res.setHeader('Access-Control-Allow-Origin', origin);
-        }else{
-            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        console.log(url.parse(origin).host);
+
+
+        if(allowedOrigins.indexOf("http://"+url.parse(origin).host) > -1 ){
+            res.setHeader('Access-Control-Allow-Origin', "http://"+url.parse(origin).host);
+        }else if(allowedOrigins.indexOf("https://"+url.parse(origin).host) > -1){
+            res.setHeader('Access-Control-Allow-Origin', "https://"+url.parse(origin).host);
         }
 
         // Request methods you wish to allow
